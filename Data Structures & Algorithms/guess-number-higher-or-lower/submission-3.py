826c4e3c252e0af5cb0832dcdf6bf025b75bf355
Angel009 <codeman24@zohomail.com>
2026-04-04 11:44:05 -0600

    Add: guess-number-higher-or-lower - submission-3

---
+++ Data Structures & Algorithms/guess-number-higher-or-lower/submission-3.py
@@ -0,0 +1,24 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        min_n = 1
+        max_n = n
+        
+        while min_n <= max_n:
+            mid_n = (min_n + max_n) // 2
+            guess_res = guess(mid_n)
+
+            if guess_res == - 1:
+                max_n = mid_n - 1
+            elif guess_res == 1:
+                min_n = mid_n + 1
+            else:
+                return mid_n
+
+        
